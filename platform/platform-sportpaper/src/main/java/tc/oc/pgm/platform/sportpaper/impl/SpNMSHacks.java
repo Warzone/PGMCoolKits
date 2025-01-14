a63950c78a06e77234a90ce135be37bff2cad64d
KaevonD <kaevon.dastpak@gmail.com>
2025-01-13 22:56:17 -0800

    this is for tank, no sleeping allowed

--- platform/platform-sportpaper/src/main/java/tc/oc/pgm/platform/sportpaper/impl/SpNMSHacks.java
+++ platform/platform-sportpaper/src/main/java/tc/oc/pgm/platform/sportpaper/impl/SpNMSHacks.java
@@ -254,7 +254,7 @@ public class SpNMSHacks implements NMSHacks {
   }
 
   @Override
-  public void setDisplayEntityRotation(Entity entity, double pitch, double yaw) {
+  public void centerBlockDisplayTransformationMatrix(Entity entity, float pitch, float yaw) {
     throw new UnsupportedOperationException("Display entities are not supported on lower versions.");
   }
 }
