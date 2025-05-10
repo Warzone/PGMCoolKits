4e8d24716045e167f44bc3b14122a88366d6d1ed
KaevonD <kaevon.dastpak@gmail.com>
2025-05-09 17:03:10 -0700

    Check between teleport locations with respect to size of projectile

--- core/src/main/java/tc/oc/pgm/projectile/ProjectileMatchModule.java
+++ core/src/main/java/tc/oc/pgm/projectile/ProjectileMatchModule.java
@@ -164,7 +164,9 @@ public class ProjectileMatchModule implements MatchModule, Listener {
                   projectile.teleport(newLocation);
                   if (projectileDefinition.damage != null || projectileDefinition.solidBlockCollision) {
                     boolean finished = false;
+                    int counter = 1;
                     while (true) {
+                      System.out.println("counter: " + counter++);
                       if (currentLocation.distanceSquared(incrementingLocation) > currentLocation.distanceSquared(newLocation)) {
                         incrementingLocation = newLocation.clone();
                         finished = true;
@@ -202,7 +204,7 @@ public class ProjectileMatchModule implements MatchModule, Listener {
                       }
 
                       if (finished) break;
-                      incrementingLocation.add(normalizedDirection);
+                      incrementingLocation.add(normalizedDirection.clone().multiply(projectileDefinition.scale));
                     }
                   }
                   return false;
